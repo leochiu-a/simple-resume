2eafe60eddd97795f145b5e7b9eee03ac767f4a7
leo <prairie5270@gmail.com>
2024-11-24 16:19:12 +0800

    refactor: change download pdf button to default variant

--- src/app/resume-editor/components/template/download-pdf-button.tsx
+++ src/app/resume-editor/components/template/download-pdf-button.tsx
@@ -27,7 +27,7 @@ const DownloadPDFButton = ({ resume }: { resume: Resume }) => {
   }, [instance.loading, instance.url, startDownload]);
 
   return (
-    <Button variant="outline" type="button" onClick={downloadResume}>
+    <Button type="button" onClick={downloadResume}>
       {instance.loading ? <LoadingSpinner /> : "Download"}
     </Button>
   );
