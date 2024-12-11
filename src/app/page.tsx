e377b999ed6a632bfd90364a6eab1c5f4c3dc800
leochiu <leochiu.life@gmail.com>
2024-12-11 14:51:28 +0800

    refactor: make spark text align center

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-4 row-start-2 items-center sm:items-start relative z-10">
-        <SparklesText text="Simple Resume" />
+        <SparklesText text="Simple Resume" className="text-center"/>
         <Typography variant="p" affects="removePMargin">
           A online tool to create a resume
         </Typography>
